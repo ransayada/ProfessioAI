44bf0111d911e9f9848909db5fe9a72a097bc994
Ran Sayada <ran.sayada@gmail.com>
2023-09-28 13:47:28 +0300

    commit #1: Initial commit, Project structure and working auth routes

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import { UserButton } from "@clerk/nextjs";
+import React from "react";
+
+const DashboardPage = () => {
+  return <div>Dashboard Page (Protected)</div>;
+};
+
+export default DashboardPage;
