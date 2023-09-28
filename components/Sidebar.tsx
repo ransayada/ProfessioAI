83353f35914054686f18128670d48200544a9267
Ran Sayada <ran.sayada@gmail.com>
2023-09-28 16:10:26 +0300

    commit #5: finish sidebar moving to the pages.

---
+++ components/Sidebar.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { Montserrat } from "next/font/google";
+import Image from "next/image";
+import Link from "next/link";
+import { sideBarRoutes } from "@/lib/constants";
+
+const montserrat = Montserrat({ weight: "600", subsets: ["latin"] });
+
+const Sidebar = () => {
+  return (
+    <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
+      <div className="px-3 py-2 flex-1">
+        <Link href="/dashboard" className="flex items-center pl-3 mb-14">
+          <div className="relative w-8 h-8 mr-4">
+            <Image fill alt="professioAI" src="/logo.png" />
+          </div>
+          <h1 className={cn("text-2xl font-bold", montserrat.className)}>
+            Profession<span className="text-indigo-500	">AI</span>
+          </h1>
+        </Link>
+        <div className="space-y-1">
+          {sideBarRoutes.map((route) => (
+            <Link
+              href={route.href}
+              key={route.href}
+              className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"
+            >
+              <div className="flex items-center flex-1">
+                <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
+                {route.label}
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ lib/constants.ts
@@ -0,0 +1,70 @@
+import { LayoutDashboard , Terminal, Building2,Utensils,BrainCog,Calculator,Scale,Cog,GraduationCap, Ratio, PlugZap, Settings } from "lucide-react";
+
+export const sideBarRoutes = [
+    {
+    label: "Dashboard",
+    icon: LayoutDashboard,
+    href: "/dashboard",
+    color: "text-indigo-500"
+},
+    {
+    label: "Software Developer",
+    icon: Terminal,
+    href: "/software-developer",
+    color: "text-indigo-500"
+},
+    {
+    label: "Chef",
+    icon: Utensils,
+    href: "/chef",
+    color: "text-indigo-500"
+},
+    {
+    label: "Psychologist",
+    icon: BrainCog,
+    href: "/psychologist",
+    color: "text-indigo-500"
+},
+    {
+    label: "Accountant",
+    icon: Calculator,
+    href: "/accountant",
+    color: "text-indigo-500"
+},
+    {
+    label: "Lawyer",
+    icon: Scale,
+    href: "/lawyer",
+    color: "text-indigo-500"
+},
+    {
+    label: "Mechanical Engineer",
+    icon: Cog,
+    href: "/mechanical-engineer",
+    color: "text-indigo-500"
+},
+    {
+    label: "Teacher",
+    icon: GraduationCap,
+    href: "/teacher",
+    color: "text-indigo-500"
+},
+    {
+    label: "Graphic Designer",
+    icon: Ratio,
+    href: "/graphic-designer",
+    color: "text-indigo-500"
+},
+    {
+    label: "Electrician",
+    icon: PlugZap,
+    href: "/electrician",
+    color: "text-indigo-500"
+},
+    {
+    label: "Settings",
+    icon: Settings,
+    href: "/settings",
+    color: "text-indigo-500"
+},
+]
