bf41489af7b3de648952d89565f7b45a1e452171
Ran Sayada <ran.sayada@gmail.com>
2023-10-29 12:12:45 +0200

    commit #17: Finish Highschool Teacher, both front route and API route .

--- app/(dashboard)/(routes)/teacher/page.tsx
+++ app/(dashboard)/(routes)/teacher/page.tsx
@@ -1,18 +1,29 @@
 "use client";
+import axios from "axios";
 import * as z from "zod";
 import AttentionFooter from "@/components/attention-footer";
 import Heading from "@/components/heading";
 import { GraduationCap } from "lucide-react";
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+import ReactMarkdown from "react-markdown";
 
 import { formSchema } from "./constants";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { ChatCompletionRequestMessage } from "openai";
+import Empty from "@/components/empty";
+import Loader from "@/components/loader";
+import { cn } from "@/lib/utils";
+import UserAvatar from "@/components/user-avatar";
+import BotAvatar from "@/components/bot-avatar";
 
 const TeacherPage = () => {
+  const router = useRouter();
+  const [messages, setMessages] = useState<ChatCompletionRequestMessage[]>([]);
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -23,14 +34,33 @@ const TeacherPage = () => {
   const isLoading = form.formState.isSubmitting;
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log(values);
+    try {
+      const userMessage: ChatCompletionRequestMessage = {
+        role: "user",
+        content: values.prompt,
+      };
+      const newMessage = [...messages, userMessage];
+
+      const response = await axios.post("/api/teacher", {
+        messages: newMessage,
+      });
+
+      setMessages((current) => [...current, userMessage, response.data]);
+
+      form.reset();
+    } catch (error: any) {
+      //TODO: Open PRO Modal
+      console.log(error);
+    } finally {
+      router.refresh();
+    }
   };
 
   return (
     <>
       <div>
         <Heading
-          title="Teacher"
+          title="High-School Teacher"
           description="Our AI teacher will help you understand subjects better, help you with your homework or assignments and make you a better student."
           icon={GraduationCap}
           iconColor="text-indigo-500"
@@ -51,7 +81,7 @@ const TeacherPage = () => {
                         <Input
                           className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
                           disabled={isLoading}
-                          placeholder="How do i calculate the radius of a circle?"
+                          placeholder="Simple toggle button using React hooks."
                           {...field}
                         />
                       </FormControl>
@@ -67,7 +97,49 @@ const TeacherPage = () => {
               </form>
             </Form>
           </div>
-          <div className="space-y-4 mt-4">Messages Content</div>
+          <div className="space-y-4 mt-4">
+            {isLoading && (
+              <div className="p-9 rounded-lg w-full flex items-center justify-center">
+                <Loader />
+              </div>
+            )}
+            {messages.length === 0 && !isLoading && (
+              <Empty label="No Test Started" />
+            )}
+            <div className="flex flex-col-reverse gap-y-4">
+              {messages.map((message) => (
+                <div
+                  key={message.content}
+                  className={cn(
+                    "p-8 w-full flex items-start gap-x-8 rounded-lg",
+                    message.role === "user"
+                      ? "bg-white border border-black/10"
+                      : "bg-muted"
+                  )}
+                >
+                  {message.role === "user" ? <UserAvatar /> : <BotAvatar />}
+                  <ReactMarkdown
+                    components={{
+                      pre: ({ node, ...props }) => (
+                        <div className="overflow-auto w-full ny-2 bg-black/10 p-2 rounded-lg">
+                          <pre {...props} />
+                        </div>
+                      ),
+                      code: ({ node, ...props }) => (
+                        <code
+                          className="bg-black/10 rounded-lg p-1"
+                          {...props}
+                        />
+                      ),
+                    }}
+                    className="text-sm overflow-hidden leading-7"
+                  >
+                    {message.content || ""}
+                  </ReactMarkdown>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
       <AttentionFooter />
---
+++ app/api/teacher/route.ts
@@ -0,0 +1,46 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import {ChatCompletionRequestMessage, Configuration,OpenAIApi} from "openai";
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+})
+
+const openai = new OpenAIApi(configuration);
+
+const instructionMessage: ChatCompletionRequestMessage = {
+    role: "system",
+    content: "As a seasoned high school teacher with two decades of experience, you approach education with a blend of passion and expertise. Tailoring your lessons to engage students, you draw inspiration from various pedagogical approaches, fostering a dynamic learning environment. Meticulously planning each class, you incorporate innovative teaching methods to cater to diverse learning styles. Your commitment to student success is evident in detailed lesson plans, clear explanations, and constructive feedback. Whether teaching classic subjects or integrating modern topics, your goal is to instill a love for learning and provide students with a solid foundation for their future endeavors."
+};
+
+export async function POST(
+    req: Request
+){
+    try{
+const {userId} = auth();
+const body = await req.json();
+const {messages} = body;
+
+if(!userId){
+    return new NextResponse("Unauthorized",{status:401})
+}
+if(!configuration.apiKey){
+    return new NextResponse("OpenAI API key is not configured",{status:500})
+}
+
+if(!messages){
+    return new NextResponse("Messages are required",{status: 400})
+}
+
+const response = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [instructionMessage,...messages]
+});
+
+return NextResponse.json(response.data.choices[0].message);
+    }
+    catch(error){
+        console.log("[TEACHER_ERROR]",error);
+        return new NextResponse("Internal error", {status: 500})
+    }
+}
