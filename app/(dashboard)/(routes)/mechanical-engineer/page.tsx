e3f38e5eb64c85d3ffb34b65871311155b799e87
Ran Sayada <ran.sayada@gmail.com>
2023-10-31 11:58:22 +0200

    commit #20: Finish Mechanical Engineer, both front route and API route .

--- app/(dashboard)/(routes)/mechanical-engineer/page.tsx
+++ app/(dashboard)/(routes)/mechanical-engineer/page.tsx
@@ -1,7 +1,132 @@
-import React from "react";
+"use client";
+import axios from "axios";
+import * as z from "zod";
+import AttentionFooter from "@/components/attention-footer";
+import Heading from "@/components/heading";
+import { Cog } from "lucide-react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+import { formSchema } from "./constants";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { ChatCompletionRequestMessage } from "openai";
+import Empty from "@/components/empty";
+import Loader from "@/components/loader";
+import { cn } from "@/lib/utils";
+import UserAvatar from "@/components/user-avatar";
+import BotAvatar from "@/components/bot-avatar";
 
 const MechanicalEngineerPage = () => {
-  return <div>MechanicalEngineerPage</div>;
+  const router = useRouter();
+  const [messages, setMessages] = useState<ChatCompletionRequestMessage[]>([]);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: "",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      const userMessage: ChatCompletionRequestMessage = {
+        role: "user",
+        content: values.prompt,
+      };
+      const newMessage = [...messages, userMessage];
+
+      const response = await axios.post("/api/mechanical-engineer", {
+        messages: newMessage,
+      });
+
+      setMessages((current) => [...current, userMessage, response.data]);
+
+      form.reset();
+    } catch (error: any) {
+      //TODO: Open PRO Modal
+      console.log(error);
+    } finally {
+      router.refresh();
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <Heading
+          title="Mechanical Engineer"
+          description="Describe the Mechanical or Technological problem and our AI Mechanical Engineer will help you explain and solve it."
+          icon={Cog}
+          iconColor="text-indigo-500"
+          bgColor="text-indigo-500/10"
+        />
+        <div className="px-4 lg:px-8">
+          <div>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="rounded-lg border w-full p-4 px-3 md:px-6 focus-within:shadow-sm grid grid-cols-12 gap-2"
+              >
+                <FormField
+                  name="prompt"
+                  render={({ field }) => (
+                    <FormItem className="col-span-12 lg:col-span-10">
+                      <FormControl className="m-0 p-0">
+                        <Input
+                          className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                          disabled={isLoading}
+                          placeholder="how to shat down the electronics in my house?"
+                          {...field}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+                <Button
+                  className="col-span-12 lg:col-span-2 w-full"
+                  disabled={isLoading}
+                >
+                  Generate
+                </Button>
+              </form>
+            </Form>
+          </div>
+          <div className="space-y-4 mt-4">
+            {isLoading && (
+              <div className="p-9 rounded-lg w-full flex items-center justify-center">
+                <Loader />
+              </div>
+            )}
+            {messages.length === 0 && !isLoading && (
+              <Empty label="No Test Started" />
+            )}
+            <div className="flex flex-col-reverse gap-y-4">
+              {messages.map((message) => (
+                <div
+                  key={message.content}
+                  className={cn(
+                    "p-8 w-full flex items-start gap-x-8 rounded-lg",
+                    message.role === "user"
+                      ? "bg-white border border-black/10"
+                      : "bg-muted"
+                  )}
+                >
+                  {message.role === "user" ? <UserAvatar /> : <BotAvatar />}
+                  <p className="text-sm">{message.content}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+      <AttentionFooter />
+    </>
+  );
 };
 
 export default MechanicalEngineerPage;
---
+++ app/api/mechanical-engineer/route.ts
@@ -0,0 +1,46 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import {ChatCompletionRequestMessage, Configuration,OpenAIApi} from "openai";
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+})
+
+const openai = new OpenAIApi(configuration);
+
+const instructionMessage: ChatCompletionRequestMessage = {
+    role: "system",
+    content: "As a seasoned electrician with two decades of hands-on experience, you approach every project with a balance of technical prowess and attention to detail. Analyzing electrical systems, you meticulously assess wiring, circuits, and components for safety and efficiency. Drawing from a deep understanding of electrical codes and standards, you navigate complex installations and troubleshoot issues with precision. Your work is marked by meticulous planning, clear documentation, and adherence to safety protocols. Whether wiring a new construction or troubleshooting electrical problems, your goal is to provide reliable, safe, and efficient electrical solutions that stand the test of time."
+};
+
+export async function POST(
+    req: Request
+){
+    try{
+const {userId} = auth();
+const body = await req.json();
+const {messages} = body;
+
+if(!userId){
+    return new NextResponse("Unauthorized",{status:401})
+}
+if(!configuration.apiKey){
+    return new NextResponse("OpenAI API key is not configured",{status:500})
+}
+
+if(!messages){
+    return new NextResponse("Messages are required",{status: 400})
+}
+
+const response = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [instructionMessage,...messages]
+});
+
+return NextResponse.json(response.data.choices[0].message);
+    }
+    catch(error){
+        console.log("[MECHANICAL_ENGINEER_ERROR]",error);
+        return new NextResponse("Internal error", {status: 500})
+    }
+}
