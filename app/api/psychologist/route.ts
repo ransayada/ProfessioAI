3b6822877b50b5af75f8fa71d591f7cdf200d851
Ran Sayada <ran.sayada@gmail.com>
2023-10-31 12:03:47 +0200

    commit #21: Finish Psychologist, both front route and API route .

---
+++ app/api/psychologist/route.ts
@@ -0,0 +1,46 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import {ChatCompletionRequestMessage, Configuration,OpenAIApi} from "openai";
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+})
+
+const openai = new OpenAIApi(configuration);
+
+const instructionMessage: ChatCompletionRequestMessage = {
+    role: "system",
+    content: "As a seasoned psychologist with two decades of clinical practice, you approach each client with a compassionate blend of expertise and empathy. Analyzing psychological complexities, you meticulously assess individual needs, employing a variety of therapeutic approaches tailored to each case. Drawing from a deep understanding of psychological theories and evidence-based practices, you navigate intricate emotional landscapes with care. Your therapeutic sessions are characterized by insightful analysis, clear communication, and a commitment to ethical standards. Whether providing counseling, conducting assessments, or contributing to research, your goal is to offer effective mental health solutions that empower individuals to lead fulfilling and meaningful lives."
+};
+
+export async function POST(
+    req: Request
+){
+    try{
+const {userId} = auth();
+const body = await req.json();
+const {messages} = body;
+
+if(!userId){
+    return new NextResponse("Unauthorized",{status:401})
+}
+if(!configuration.apiKey){
+    return new NextResponse("OpenAI API key is not configured",{status:500})
+}
+
+if(!messages){
+    return new NextResponse("Messages are required",{status: 400})
+}
+
+const response = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [instructionMessage,...messages]
+});
+
+return NextResponse.json(response.data.choices[0].message);
+    }
+    catch(error){
+        console.log("[PSYCHOLOGIST_ERROR]",error);
+        return new NextResponse("Internal error", {status: 500})
+    }
+}
